710aab3598cb9052b9c11927facdf49d5bbbd74f
Rakuraku Jyo <jyo.rakuraku@gmail.com>
2013-08-15 12:43:22 +0900

    Move flickr to a service

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -1,20 +1,15 @@
 'use strict';
 
 angular.module('angularTestApp')
-    .controller('AboutCtrl', function($scope, $http) {
-      var apiUrl = 'http://api.flickr.com/services/rest/?method=flickr.tags.getClusterPhotos&api_key=4c2a311024775871cc745b9338ce894a&tag=tokyo';
-      $http({method: 'JSONP', url: apiUrl + '&format=json&jsoncallback=JSON_CALLBACK'}).success(function(data) {
-        $scope.page = data.photos.page;
-        $scope.pages = data.photos.pages;
-        $scope.perpage = data.photos.perpage;
-        $scope.total = data.photos.total;
-        $scope.photos = data.photos.photo;
-      });
-
+    .controller('AboutCtrl', function($scope, $http, flickrAPI) {
       $scope.filterTitle = function(photo) {
         var re = new RegExp($scope.query, 'ig');
         return re.test(photo.title);
       };
+
+      flickrAPI.getClusterPhotos(function(photos) {
+        $scope.photos = photos;
+      });
 
       $scope.currentTab = 'about';
     });
--- app/scripts/services/flickr.js
+++ app/scripts/services/flickr.js
@@ -1,7 +1,18 @@
 'use strict';
 
 angular.module('angularTestApp')
-  .service('flickr', function flickr() {
-    // AngularJS will instantiate a singleton by calling "new" on this function
+    .service('flickrAPI', function flickr($http) {
+      // AngularJS will instantiate a singleton by calling "new" on this function
+      function getClusterPhotos(callback) {
+        var key = '911663d70af9a5745cc79545285ae93e';
+        var apiUrl = 'http://api.flickr.com/services/rest/?method=flickr.tags.getClusterPhotos&api_key=' + key +
+            '&tag=mac';
+        $http({method: 'JSONP', url: apiUrl + '&format=json&jsoncallback=JSON_CALLBACK'}).success(function(data) {
+          callback(data.photos.photo);
+        });
+      }
 
-  });
+      return {
+        getClusterPhotos: getClusterPhotos
+      };
+    });
